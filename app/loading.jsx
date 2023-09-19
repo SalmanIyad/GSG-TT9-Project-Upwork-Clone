ca643c5a47f2cb1970a28e329390395ef17fde51
SalmanIyad <alhinsalman40@gmail.com>
2023-09-19 13:01:01 +0300

    Loading, NotFound Pages Enhancments

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,17 @@
+import { Button } from "@mui/joy";
 import React from "react";
 
 export default function loading() {
   return (
-    <div className="text-center">
-      <p className="text-2xl">loading...</p>
+    <div className="text-center my-32">
+      <Button 
+        loading 
+        variant="plain"
+        size="xl"
+        color="success"
+        >
+        Plain
+      </Button>
     </div>
   );
 }
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div className='text-center'>
-      <p className='text-8xl'>404</p>
-      <h2>Page Not Found</h2>
+    <div className='text-center my-32'>
+      <p className='text-8xl text-[#108A00]'>404</p>
+      <h3>Page Not Found</h3>
       <p>Could not find requested resource</p>
-      <Link className='underline hover:text-white hover:no-underline transition-all duration-100' href="/">Return Home</Link>
+      <Link className='underline text-[#108A00] hover:text-[#225d13] hover:no-underline transition-all duration-100' href="/">Return Home</Link>
     </div>
   )
 }
